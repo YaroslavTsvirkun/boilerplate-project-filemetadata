f2d772f9f735d89b9bbcf32df5b3461ac8a7358f
YaroslavTsvirkun <yaroslav.tsvirkun.1992@outlook.com>
2023-10-08 14:44:17 +0000

    change index.js, added implementation File Metadata Microservice

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
